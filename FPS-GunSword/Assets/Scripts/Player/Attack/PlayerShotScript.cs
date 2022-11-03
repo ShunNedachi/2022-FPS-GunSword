ecc49f93b13adab635e901a1585b33bcb8d66d14
newshell <52154054+newshell3125@users.noreply.github.com>
2022-11-03 14:47:29 +0900

    射撃周り

--- FPS-GunSword/Assets/Scripts/Player/Attack/PlayerShotScript.cs
+++ FPS-GunSword/Assets/Scripts/Player/Attack/PlayerShotScript.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class PlayerShotScript : MonoBehaviour
 {
     public float rayDistance;
+    [SerializeField] private int magazineMax;
+    [SerializeField] private float shootInterval = 3.0f;
+    [SerializeField] private float reloadInterval = 3.0f;
+
+    private float shootIntervalTimer = 0;
+    private float reloadTimer = 0;
+    private int magazine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        magazine = magazineMax;
+    }
 
     private void Update()
     {
         Transform trans = transform;
+        timer += Time.deltaTime;
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && magazine>0 &&shootIntervalTimer >= shootInterval)
         {
             var direction = trans.forward;
 
             Vector3 rayPosition = trans.position + new Vector3(0.0f, 0.0f, 0.0f);
             Ray ray = Camera.main .ScreenPointToRay(Input.mousePosition);
             Debug.DrawRay(rayPosition, direction * -rayDistance, UnityEngine.Color.red);
+
+            magazine--;
 
             RaycastHit hit;
             if(Physics.Raycast(ray,out hit))
@@ -28,7 +44,18 @@ public class PlayerShotScript : MonoBehaviour
                     Destroy(hit.collider.gameObject);
                 }
             }
-
         }
+
+        if(Input.GetKeyDown(KeyCode.R) && reloadTimer >= reloadInterval)
+        {
+            // タイマーの初期化
+            timer = 0;
+            Reload();
+        }
+    }
+
+    private void Reload()
+    {
+        magazine = magazineMax;
     }
 }
