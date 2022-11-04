0f66c7687d5d50a86d088f7eb9e6c3123e224133
newshell <52154054+newshell3125@users.noreply.github.com>
2022-11-04 15:52:19 +0900

    スタミナ周りや移動系を変更

---
+++ FPS-GunSword/Assets/Scripts/Player/Attack/Melee/PlayerSlashScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class PlayerSlashScript : MonoBehaviour
+{
+    public static PlayerSlashScript instance;
+    
+    [SerializeField] private GameObject enemy;
+    
+    private int comboCount = 0;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            Vector3 enemyPos = enemy.transform.position;
+            enemyPos.y = transform.position.y;
+            if(Vector3.Distance(enemyPos,transform.position) <= 2.0f && Vector3.Dot(Vector3.Normalize(enemyPos-transform.position),transform.forward)>= 0.8f)
+            {
+                //damage処理
+            }
+        }
+    }
+
+    public int GetComboCount()
+    {
+        return comboCount;
+    }
+}
---
+++ FPS-GunSword/Assets/Scripts/Player/status/PlayerEnergyScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEnergyScript : MonoBehaviour
+{
+    [SerializeField] private int energyMax;
+    private float energy = 0;
+    private int energyItem = 60;
+    private int baceEnergyAbsorption = 25;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetEnergyItem()
+    {
+        if(energy<energyMax)
+        {
+            energy += energyItem;
+        }
+    }
+
+    public void SlashChargeEnergy()
+    {
+        if(PlayerSlashScript.instance.GetComboCount() == 0)
+        {
+            energy += baceEnergyAbsorption;
+        }
+        else
+        {
+            energy = energy + PlayerSlashScript.instance.GetComboCount() * baceEnergyAbsorption * 1.1f;
+        }
+    }
+}
