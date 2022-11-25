a26ff4015a67e6206e82a167436702a89775c6b3
newshell <52154054+newshell3125@users.noreply.github.com>
2022-11-25 21:40:06 +0900

    学校終了時までの更新データ

--- FPS-GunSword/Assets/Scripts/Player/Attack/Melee/AttackRange.cs
+++ FPS-GunSword/Assets/Scripts/Player/Attack/Melee/AttackRange.cs
@@ -19,6 +19,8 @@ public class AttackRange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.position = PlayerSlashScript.instance.GetAttackPos();
+
         if(destroyTimer>DestroyInterval)
         {
             if(hit == false)
