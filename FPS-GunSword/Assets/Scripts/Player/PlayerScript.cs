ecdc2ec611a58e8ead2fe50aa3ab4ed08522e3b9
sioyuu <k019g1384@g.neec.ac.jp>
2022-11-25 14:32:15 +0900

    スタミな追加

--- FPS-GunSword/Assets/Scripts/Player/PlayerScript.cs
+++ FPS-GunSword/Assets/Scripts/Player/PlayerScript.cs
@@ -30,7 +30,7 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         PlayerEnergyScript.instance.Update();
-
+        PlayerStaminaScript.instance.Update();
         PlayerMagazineScript.instance.Update();
 
         if(Input.GetKeyDown(KeyCode.F) && PlayerEnergyScript.instance.GetULTchack())
