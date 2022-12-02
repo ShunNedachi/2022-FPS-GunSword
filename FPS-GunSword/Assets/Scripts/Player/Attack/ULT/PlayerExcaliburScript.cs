2e08b406973239084d59825641525a0d0b5dbbf1
newshell <52154054+newshell3125@users.noreply.github.com>
2022-12-02 16:10:54 +0900

    Excalibur下地

--- FPS-GunSword/Assets/Scripts/Player/Attack/ULT/PlayerExcaliburScript.cs
+++ FPS-GunSword/Assets/Scripts/Player/Attack/ULT/PlayerExcaliburScript.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerULTScript : MonoBehaviour
+public class PlayerExcaliburScript : MonoBehaviour
 {
-    public static PlayerULTScript instance;
-    
+    public static PlayerExcaliburScript instance;
     [SerializeField] private int attackInterval = 50;
     [SerializeField] private float attackDictance = 5.0f;
     [SerializeField] private new GameObject camera;
-    [SerializeField] private GameObject ULTAttackRange;
+    [SerializeField] private GameObject Excalibur;
 
     private float attackTimer = 0;
+    private float objectRange = 0;
     private bool action = false;
 
     public void Awake()
@@ -26,32 +26,26 @@ public class PlayerULTScript : MonoBehaviour
     public void Start()
     {
         attackTimer = attackInterval;
+
     }
 
     // Update is called once per frame
     public void Update()
     {
         attackTimer++;
-        if(PlayerExcaliburScript.instance.GetAction() == false)
+        if(PlayerULTScript.instance.GetAction() == false)
         {
-            if(Input.GetMouseButton(1) && attackTimer > attackInterval )
+            if(Input.GetMouseButton(0) && attackTimer > attackInterval )
             {
-                action =  true;
+                action = true;
                 PlayerEnergyScript.instance.EnemyConsumptionSlash();
                 attackTimer = 0;
                 // プレイヤーの少し前に生成する
-                Vector3 createPos = transform.position + camera.transform.forward * attackDictance;
-                Instantiate(ULTAttackRange, createPos, camera.transform.rotation);
+                Vector3 createPos = transform.position + camera.transform.forward * Excalibur.GetComponent<Renderer>().bounds.size.z / 2.0f;
+                Instantiate(Excalibur, createPos, camera.transform.rotation);
             }
         }
     }
-    public Vector3 GetAttackPos()
-    {
-        Vector3 createPos = transform.position + camera.transform.forward * attackDictance;
-
-        return createPos;
-    }
-
     public bool GetAction()
     {
         return action;
